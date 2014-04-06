efc7c8c70cde818c45094e6a667f127e2b907e2b
Bob Stoute <stoute@planet.nl>
2014-04-06 11:17:00 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-    angular.module("ngApp",["ngApp.ng_weather","ui.bootstrap","stoute.ng-baseApp","stoute.ng-drupalService","stoute.ng-swipe","stoute.require","ngApp.controllers","ngApp.directives","ngApp.services","ngApp.ng_block_1","ngApp.ng_block_2","ngApp.ng_block_3"])
+    angular.module("ngApp",["ui.bootstrap","stoute.ng-baseApp","stoute.ng-drupalService","stoute.ng-drupalService.controllers","stoute.ng-drupalService.directives","stoute.ng-drupalService.filters","stoute.ng-swipe","stoute.require","ngApp.ng_block_1","ngApp.ng_block_2","ngApp.ng_block_3","ngApp.ng_weather"])
         .controller("ngAppCtrl", function($scope,$http,AppService,AppSettings,DrupalService) {
             AppService.initialize({
                 config_file: window.Drupal.settings.ng_app.config_file,
--- ng_block_3/ng_block_3.js
+++
@@ -1,9 +0,0 @@
-angular.module('ngApp.ng_block_3', [])
-
-    .controller('ng_block_3Ctrl', function($scope,AppSettings,$http,$log,Nodes,Node,Views) {
-        $scope.$on('init', function (e,appScope) {
-            $scope.model = Drupal.settings.ng_block_3.model;
-        })
-    })
-
-;
